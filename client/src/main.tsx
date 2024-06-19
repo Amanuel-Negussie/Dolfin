2ff4875c40b395c31837a5f4130fd0ac8d177ceb
Cyril Kamal <43249970+CyrilKamal@users.noreply.github.com>
2024-06-19 11:11:44 -0400

    deleted theme tage and added root react router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
 import Auth0ProviderWithNavigate from "./utils/Auth0ProviderWithNavigate";
 // import './index.css'
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Auth0ProviderWithNavigate>
         <App />
       </Auth0ProviderWithNavigate>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
