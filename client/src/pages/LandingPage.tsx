cbf520355f9e4ae363e395d4b4a4b084ca26a1ae
Cyril Kamal <43249970+CyrilKamal@users.noreply.github.com>
2024-06-28 04:19:09 -0400

    Updated message on left

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -75,11 +75,11 @@ function LandingPage() {
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
+                &ldquo;This app has saved me countless hours of work and
+                helped me get financial information faster than
                 ever before.&rdquo;
               </p>
-              <footer className="text-sm">Sofia Davis</footer>
+              <footer className="text-sm">Cyril Kamal</footer>
             </blockquote>
           </div>
         </div>
