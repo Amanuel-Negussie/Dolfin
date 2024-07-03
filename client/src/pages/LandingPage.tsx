cd258b0714d4732370ec861053da59f77ef4dd85
Cyril Kamal <43249970+CyrilKamal@users.noreply.github.com>
2024-07-03 09:50:11 -0400

    Added img backdrop to screen

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -5,15 +5,16 @@ import { Link } from "react-router-dom";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 import { UserAuthForm } from "@/components/user-auth-form";
-
+import bankImage from '/imgs/bank.jpg'
 function LandingPage() {
   const { isAuthenticated } = useAuth0();
 
   useEffect(() => {
     document.title = "Authentication";
-    const metaDescription = document.createElement('meta');
-    metaDescription.name = 'description';
-    metaDescription.content = 'Authentication forms built using the components.';
+    const metaDescription = document.createElement("meta");
+    metaDescription.name = "description";
+    metaDescription.content =
+      "Authentication forms built using the components.";
     document.head.appendChild(metaDescription);
 
     return () => {
@@ -56,7 +57,11 @@ function LandingPage() {
           Login
         </Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-          <div className="absolute inset-0 bg-zinc-900" />
+          <img
+            src={bankImage}
+            alt="Background"
+            className="absolute inset-0 object-cover w-full h-full"
+          />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -75,9 +80,8 @@ function LandingPage() {
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                &ldquo;This app has saved me countless hours of work and
-                helped me get financial information faster than
-                ever before.&rdquo;
+                &ldquo;This app has saved me countless hours of work and helped
+                me get financial information faster than ever before.&rdquo;
               </p>
               <footer className="text-sm">Cyril Kamal</footer>
             </blockquote>
