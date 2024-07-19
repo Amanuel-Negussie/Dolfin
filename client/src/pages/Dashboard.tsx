843e776fd1d0c81b7e10d1f6a08e1033f755c516
Jacky Lam <Jlam5676@conestogac.on.ca>
2024-07-19 11:08:34 -0400

    DT-30 Integrated transaction card with backend.

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,13 +1,16 @@
 import { AccountSummaryCard } from "@/components/AccountSummaryCard";
 import { TransactionTrendsCard } from "@/components/TransactionTrendsCard";
 import { TransactionsCard } from "@/components/TransactionsCard";
+import { Transaction } from "@/components/types";
 import useLogin from "@/hooks/useLogin";
 import { useAccounts, useTransactions } from "@/services";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Dashboard: React.FC = () => {
     const { getTransactionsByUser, transactionsByUser } = useTransactions();
     const { getAccountsByUser, accountsByUser } = useAccounts();
+    const [isLoading, setIsLoading] = useState(true);
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
     const userId = Number(useLogin());
 
     useEffect(() => {
@@ -19,15 +22,20 @@ export const Dashboard: React.FC = () => {
 
     useEffect(() => {
         if(Object.keys(transactionsByUser).length > 0) {
-            console.log(transactionsByUser);
+            setTransactions(Object.values(transactionsByUser)[0]);
+            setIsLoading(false);
         }
     }, [transactionsByUser]);
 
     useEffect(() => {
         if(Object.keys(accountsByUser).length > 0) {
-            console.log(accountsByUser);
+            //console.log(accountsByUser);
         }
     }, [accountsByUser]);
+
+    if(isLoading) {
+        return <div>Loading...</div>
+    }
 
     return (
         <div className="hidden flex-col md:flex">
@@ -39,7 +47,7 @@ export const Dashboard: React.FC = () => {
                 <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-2">
                     <TransactionTrendsCard />
 
-                    <TransactionsCard />
+                    <TransactionsCard transactions={transactions}/>
                 </div>
             </div>
         </div>
