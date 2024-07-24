8ebb7b04f4758de24b98308d5852a0b31b6dfd8c
Amanuel <anegussie9077@conestogac.on.ca>
2024-07-24 15:51:50 -0400

    update to the tests :)

--- db/tests/tests/transactions-tests.sql
+++ db/tests/tests/transactions-tests.sql
@@ -1,14 +1,14 @@
 -- TEST 1 --- 
 IF 
 (SELECT TOP 1 Amount FROM dbo.transactions WHERE account_id = 1)=100.00 
-PRINT 'Transaction Amount 100.00 is validated'
-ELSE THROW 50000, '100.00 expected but received', 1;
+PRINT 'Transactions Test 1: Transaction Amount 100.00 is validated'
+ELSE THROW 50000, 'Transactions Test 1 100.00 expected', 1;
 GO
 
 --- TEST 2 ---
 
 IF 
 (SELECT TOP 1 Amount FROM dbo.transactions WHERE account_id = 2)=50.00 
-PRINT 'Transaction Amount 100.00 is validated'
-ELSE THROW 50000, '100.00 expected but received', 1;
+PRINT 'Transactions Test 2: Transaction Amount 50.00 is validated'
+ELSE THROW 50000, 'Transactions Test 2: 50.00 expected', 1;
 GO
--- db/tests/tests/users-tests.sql
+++ db/tests/tests/users-tests.sql
@@ -1,4 +1,4 @@
 IF (SELECT TOP 1 username FROM users_table WHERE auth0_id = 'auth0|1234567890') = 'john_doe' 
-PRINT 'This is a success' 
-ELSE THROW 50000, 'This is a failure', 1;
+PRINT 'Users-Test 1: This is a success' 
+ELSE THROW 50000, 'Users-Test 1: This is a failure', 1;
 GO
